607ec4f7e2cdf5487b7463b6257da3725ac77c31
SemenSavlook <semensavluk@gmail.com>
2023-11-30 20:01:54 +0600

    cont

--- src/js/index.js
+++ src/js/index.js
@@ -2,23 +2,31 @@ const closeButtons = document.querySelectorAll('.js-modal-phone-close');
 const phoneButton = document.querySelector('#js-modal-phone-button');
 const modal = document.querySelector('#js-modal-phone');
 
-
 const overflow = document.createElement('div');
-overflow.classList.add('overflow-body')
+overflow.classList.add('overflow-body');
 
 phoneButton.addEventListener('click', () => {
   document.body.style.paddingRight = `${window.innerWidth - document.documentElement.clientWidth}px`;
-  modal.classList.add('visible');
   document.body.style.overflow = 'hidden';
   modal.before(overflow)
-  console.log(overflow)
-})
+  modal.classList.add('visible');
+  window.addEventListener('mouseup', closeHandler);
+});
+
+function closeModal() {
+  modal.classList.remove('visible');
+  document.body.style.overflow = '';
+  overflow.remove();
+  document.body.style.paddingRight = 0;
+  window.removeEventListener('mouseup', closeHandler);
+}
 
 closeButtons.forEach((el) => {
-  el.addEventListener('click', () => {
-    modal.classList.remove('visible');
-    document.body.style.overflow = '';
-    overflow.remove();
-    document.body.style.paddingRight = 0;
-  })
-})
+  el.addEventListener('click', closeModal);
+});
+
+function closeHandler(e) {
+  if (!e.target.closest('#js-modal-phone')) {
+    closeModal()
+  }
+}
