327eedcb451e765054c4b5ad1167dbbdf66cf873
SemenSavlook <semensavluk@gmail.com>
2023-12-04 02:31:34 +0600

    add swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,13 @@
 import modalPhoneHandler from "./modules/modal-phones";
-modalPhoneHandler()
+import Swiper from 'swiper';
+import 'swiper/css';
+
+// eslint-disable-next-line no-unused-vars
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  speed: 1000,
+  spaceBetween: 20,
+  autoplay: true,
+});
+
+modalPhoneHandler();
