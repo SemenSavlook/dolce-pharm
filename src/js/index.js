db249a2a34a6c9657f44469419f073998c95ae61
SemenSavlook <semensavluk@gmail.com>
2023-12-19 19:11:59 +0600

    done pre build

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import modalPhoneHandler from "./modules/modal-phones";
 import modalRequestPrice from "./modules/modal-request";
+import orderButtonHandler from "./modules/order-Button-handler";
+
 import Swiper from 'swiper';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 
@@ -28,16 +30,13 @@ window.addEventListener('DOMContentLoaded', () => {
     },
   });
 
-  const mobSwiper = new Swiper('.mobSwiper', {
-    modules: [ Navigation, Pagination, Autoplay ],
-    loop: true,
-    speed: 2000,
-    spaceBetween: 10,
-    autoplay: false
-  });
+  window.addEventListener('resize', checkWindowSize);
+
 
   modalPhoneHandler(swiper);
   modalRequestPrice(swiper);
+  checkWindowSize();
+  orderButtonHandler('.js-getOrder-button', 'https://wa.me/+77711041201');
 
   Fancybox.bind("[data-fancybox]", {
     Toolbar: {
@@ -49,3 +48,27 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 
 })
+
+function checkWindowSize() {
+  if (window.innerWidth < 576) {
+    var mobSwiper = new Swiper('.mobSwiper', {
+      modules: [ Navigation, Pagination, Autoplay ],
+      loop: false,
+      speed: 1500,
+      spaceBetween: 10,
+      autoplay: {
+        delay: 6000,
+        stopOnLastSlide: true,
+        pauseOnMouseEnter: true
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
+    });
+  } else {
+    if (mobSwiper) {
+      mobSwiper.destroy();
+    }
+  }
+}
---
+++ src/js/modules/order-Button-handler.js
@@ -0,0 +1,17 @@
+export default function orderButtonHandler(selector, waNumber) {
+  const orderButtons = document.querySelectorAll(selector);
+  if (orderButtons && orderButtons.length > 0) {
+    orderButtons.forEach((el) => {
+      el.addEventListener('click', () => {
+        var link = document.createElement("a");
+        link.classList.add('TEST')
+        link.href = waNumber;
+        link.target = "_blank";
+        link.click();
+        link = null;
+      })
+    })
+  } else {
+    console.log('No order buttons')
+  }
+}
